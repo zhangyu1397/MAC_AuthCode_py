edf7671dccf034cbe183030a9419f088ebdb6e0c
zhangyu1397 <753422272@qq.com>
2018-01-06 00:09:27 +0800

    write

--- File/File.py
+++ File/File.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
-class FIle:
+class File:
     __fd = None
     def __init__(self):
 
-        with open('MAC-AuthCode.txt', 'a+') as self.__fd:
+        with os.open('MAC-AuthCode.txt', 'a+') as self.__fd:
             print('open MAC-AuthCode success\n')
 
     def write_mac(self, wirtestring):
--- main.py
+++ main.py
@@ -2,10 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from NetBrocastUdp.NetBroadcastUdp import BroadcastUdp
+from File.File import File
+import queue
+import threading
 
-#class Controller:
+class MainController:
 
-    #def __init__(self):
+    def __init__(self):
+        self._sys_init()
+        self._run()
+        self.dataque = queue.Queue
+
+    def _sys_init(self):
+        self.broadcast = BroadcastUdp
+        self.file = File
+
+    def _run(self):
+        threading.Thread(target=self._pth_run)
+        while True:
+            data = self.dataque.get()
+            self.file.write_mac(data)
+
+    def _pth_run(self):
+        while True:
+            data = self.broadcast.get_broadcast_data()
+            self.dataque.put(data)
+
         #打开文件
         #建立服务器
         #建立广播程序
